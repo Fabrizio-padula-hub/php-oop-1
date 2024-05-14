8c20634d8a66420a5f193d5dab4829a34876feba
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-05-14 15:36:43 +0200

    feat: adding instance variables

--- index.php
+++ index.php
@@ -7,11 +7,23 @@ class Movie{
 
 }
 
+$independenceDay = new Movie();
+$independenceDay->title = 'Independence Day';
+$independenceDay->director = 'Roland Emmerich';
+$independenceDay->genre = 'fantascienza';
+$independenceDay->year = '1996';
+
+$interstellar = new Movie();
+$interstellar->title = 'interstellar';
+$interstellar->director = 'Christopher Nolan';
+$interstellar->genre = 'fantascienza';
+$interstellar->year = '2014';
 
 
 
 
-
+var_dump($independenceDay);
+var_dump($interstellar);
 
 
 ?>
